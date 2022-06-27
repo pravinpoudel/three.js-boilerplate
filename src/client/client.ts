e71ee33fe19f4cfbe18cf12ee37fbf6ea89f4f5a
pravinpoudel <prvnpoudel4@gmail.com>
2022-06-27 16:24:34 -0600

    three.js boilerplate created

---
+++ src/client/client.ts
@@ -0,0 +1,61 @@
+import "./styles/style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+const scene = new THREE.Scene();
+
+const camera1 = new THREE.PerspectiveCamera(75, 1, 0.1, 10);
+const camera2 = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10);
+const camera3 = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10);
+const camera4 = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 10);
+
+camera1.position.z = 2;
+camera2.position.y = 1;
+camera2.lookAt(new THREE.Vector3(0, 0, 0));
+camera3.position.z = 1;
+camera4.position.x = 1;
+camera4.lookAt(new THREE.Vector3(0, 0, 0));
+
+const canvas1 = document.getElementById("c1") as HTMLCanvasElement;
+const canvas2 = document.getElementById("c2") as HTMLCanvasElement;
+const canvas3 = document.getElementById("c3") as HTMLCanvasElement;
+const canvas4 = document.getElementById("c4") as HTMLCanvasElement;
+const renderer1 = new THREE.WebGLRenderer({ canvas: canvas1 });
+renderer1.setSize(200, 200);
+const renderer2 = new THREE.WebGLRenderer({ canvas: canvas2 });
+renderer2.setSize(200, 200);
+const renderer3 = new THREE.WebGLRenderer({ canvas: canvas3 });
+renderer3.setSize(200, 200);
+const renderer4 = new THREE.WebGLRenderer({ canvas: canvas4 });
+renderer4.setSize(200, 200);
+
+//document.body.appendChild(renderer.domElement)
+
+new OrbitControls(camera1, renderer1.domElement);
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({
+  color: 0x00ff00,
+  wireframe: true,
+});
+
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  render();
+}
+
+function render() {
+  renderer1.render(scene, camera1);
+  renderer2.render(scene, camera2);
+  renderer3.render(scene, camera3);
+  renderer4.render(scene, camera4);
+}
+
+animate();
---
+++ src/client/webpack.common.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+  entry: "./src/client/client.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "../../dist/client"),
+  },
+};

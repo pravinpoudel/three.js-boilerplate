d56c88deeb6e36687b6825c34029ed2749489178
pravinpoudel <prvnpoudel4@gmail.com>
2022-06-28 02:30:19 -0600

    https server instead of http

--- src/client/webpack.dev.js
+++ src/client/webpack.dev.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const path = require("path");
@@ -8,6 +9,10 @@ module.exports = merge(common, {
   devServer: {
     static: {
       directory: path.join(__dirname, "../../dist/client"),
+    },
+    https: {
+      key: fs.readFileSync(path.join(__dirname, "../../key.pem")),
+      cert: fs.readFileSync(path.join(__dirname, "../../cert.pem")),
     },
     hot: true,
   },
